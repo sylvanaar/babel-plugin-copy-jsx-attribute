3e797c4bf6902a06ba54d06b3a91adbfbc4c5009
sylvanaar <sylvanaar@mindspring.com>
2020-09-19 15:24:31 -0700

    🔇 Remove logs

--- src/index.js
+++ src/index.js
@@ -9,12 +9,9 @@ export default function ({types: t}) {
       JSXAttribute(path) {
         if (path.container.some((a) => a.name.name === DEST_ATTRIBUTE)) return;
         path.container.forEach((a) => {
-          console.log(a);
           if (a.name.name !== SOURCE_ATTRIBUTE) return;
-
           const identifier = t.jSXIdentifier(DEST_ATTRIBUTE);
           const attr = t.jSXAttribute(identifier, a.value);
-          console.log("new", attr);
           path.container.push(attr);
           return;
         });
